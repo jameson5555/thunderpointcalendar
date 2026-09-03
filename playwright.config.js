c0a97d7f0851e480798a23fcef6044e0e10baec4
Jameson <jm@ten2.media>
2026-09-03 15:53:27 -0700

    fix for ci

--- playwright.config.js
+++ playwright.config.js
@@ -35,12 +35,12 @@ export default defineConfig({
             timeout: 120_000,
             env: process.env,
         },
-        {
+        ...(process.env.CI ? [] : [{
             command: 'npm run dev -- --host 127.0.0.1 --port 5173',
             url: 'http://127.0.0.1:5173/@vite/client',
             reuseExistingServer: false,
             timeout: 120_000,
             env: process.env,
-        },
+        }]),
     ],
 });
